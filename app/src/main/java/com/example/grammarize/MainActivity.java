5801456086319904bcc27c9fc0bf75c05bfb480e
Chinay <55807470+chinay-domitrix@users.noreply.github.com>
2022-05-21 16:15:03 -0400

    :wave:
    Signed-off-by: Chinay <55807470+Chinay-Domitrix@users.noreply.github.com>

--- app/src/main/java/com/example/grammarize/MainActivity.java
+++ app/src/main/java/com/example/grammarize/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.grammarize;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.grammarize.R.layout.activity_main;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	    super.onCreate(savedInstanceState);
+	    setContentView(activity_main);
     }
 }
