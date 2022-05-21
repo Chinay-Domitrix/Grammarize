e70e25a9ffff29114bf70d226ceef02561c2281c
Chinay <55807470+chinay-domitrix@users.noreply.github.com>
2022-05-21 16:27:53 -0400

    :wave:
    Signed-off-by: Chinay <55807470+Chinay-Domitrix@users.noreply.github.com>

--- app/src/main/java/com/example/grammarize/MainActivity.java
+++ app/src/main/java/com/example/grammarize/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.grammarize;
 
 import static com.example.grammarize.R.layout.activity_main;
+import static com.example.grammarize.R.string.gpt3;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+	String gpt3APIKey = getString(gpt3);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
